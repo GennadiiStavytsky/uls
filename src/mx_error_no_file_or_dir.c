63511c85906296f1aceb89e414c6002a5db2cb9f
Gennadii <stavytskyigennadii@gmail.com>
2021-02-08 13:44:38 +0200

    Add files via upload

---
+++ src/mx_error_no_file_or_dir.c
@@ -0,0 +1,8 @@
+#include "uls.h"
+
+void mx_error_no_file_or_dir(char *name) {
+    mx_printerr("uls: ");
+    mx_printerr(name);
+    mx_printerr(": No such file or directory\n");
+    errno = 1;
+}
