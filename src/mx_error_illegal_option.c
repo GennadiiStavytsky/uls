950f2b8c285930347c8252e986657b312658cef2
Gennadii <stavytskyigennadii@gmail.com>
2021-02-08 13:43:13 +0200

    Add files via upload

---
+++ src/mx_error_illegal_option.c
@@ -0,0 +1,10 @@
+#include "uls.h"
+
+void mx_error_illegal_option(char c) {
+    mx_printerr("uls: illegal option -- ");
+    mx_printchar_error(c);
+    mx_printerr("\nusage: uls [-");
+    mx_printerr(FLAGS);
+    mx_printerr("] [file ...]\n");
+    errno = 1;
+}
