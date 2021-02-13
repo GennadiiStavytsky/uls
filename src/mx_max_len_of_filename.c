db0cdc9a0e949d6e1b96faa1f4d56c50b379aaf6
Gennadii <stavytskyigennadii@gmail.com>
2021-02-13 23:14:27 +0200

    Add files via upload

---
+++ src/mx_max_len_of_filename.c
@@ -0,0 +1,11 @@
+#include "uls.h"
+
+int mx_max_len_of_filename(char **objects, int num_in_dir) {
+    int max_len = mx_s_strlen(objects[0]);
+
+    for (int i = 1; i < num_in_dir; i++) {
+        mx_strlen(objects[i]) > max_len ? max_len = mx_strlen(objects[i]) : i;
+    }
+
+    return max_len = max_len - max_len % 8 + 8;
+}
