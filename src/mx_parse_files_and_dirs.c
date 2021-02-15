ec4f58668ff8962cccda50d9cc6f8536321ef6b7
Gennadii <stavytskyigennadii@gmail.com>
2021-02-16 00:27:04 +0200

    Add files via upload

---
+++ src/mx_parse_files_and_dirs.c
@@ -0,0 +1,68 @@
+#include "uls.h"
+
+static void check_file_type(t_uls **uls, char **dont_exist,
+                                          int i, struct stat sb) {
+    if (S_ISFIFO(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISCHR(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISDIR(sb.st_mode))
+        (*uls)->dirs[(*uls)->count_dirs++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISBLK(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISREG(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISLNK(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else if (S_ISSOCK(sb.st_mode))
+        (*uls)->files[(*uls)->count_files++] = mx_strdup((*uls)->all_files[i]);
+    else {
+        (*dont_exist) = mx_strdup((*uls)->all_files[i]);
+        (*uls)->wrong++;
+    }
+}
+
+
+static void save_file_and_dirs(t_uls **uls, int diff) {
+    struct stat sb;
+    char **dont_exist = (char **)malloc(sizeof(char *) * diff + 1);
+
+    (*uls)->count_dirs = 0;
+    (*uls)->count_files = 0;
+    (*uls)->wrong = 0;
+    for (int k = 0; k <= diff; dont_exist[k++] = NULL);
+    for (int i = 0; (*uls)->all_files[i] != NULL; i++) {
+        sb.st_mode = 0;
+        if ((*uls)->flag_l == true)
+            lstat((*uls)->all_files[i], &sb);
+        else
+            stat((*uls)->all_files[i], &sb);
+        check_file_type(uls, &dont_exist[(*uls)->wrong], i, sb);
+    }
+    if (((*uls)->flags && !mx_strchr((*uls)->flags, 'f')) || !(*uls)->flags)
+        mx_bubble_sort(dont_exist, (*uls)->wrong);
+    for (int j = 0; j < (*uls)->wrong; j++)
+        mx_error_no_file_or_dir(dont_exist[j]);
+    mx_del_strarr(&dont_exist);
+}
+
+
+void mx_parse_files_and_dirs(t_uls **uls, char **argv, int argc, int count) {
+    int diff = argc - count;
+    (*uls)->count_all_files = diff;
+
+    (*uls)->all_files = (char **) malloc(sizeof(char *) * diff + 1);
+    for (int i = 0; i <= diff; (*uls)->all_files[i++] = NULL);
+
+    for (int i = 0; count < argc; i++) {
+        (*uls)->all_files[i] = mx_strdup(argv[count]);
+        count++;
+    }
+    (*uls)->dirs = (char **) malloc(sizeof(char *) * diff + 1);
+    (*uls)->files = (char **) malloc(sizeof(char *) * diff + 1);
+    for (int i = 0; i <= diff; i++) {
+        (*uls)->dirs[i] = NULL;
+        (*uls)->files[i] = NULL;
+    }
+    save_file_and_dirs(uls, diff);
+}
